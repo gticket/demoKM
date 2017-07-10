c70fbb5880a68064089bd343d5651005875603db
Glenn Ticket <glenn.ticket@wolterskluwer.com>
2017-07-10 16:30:05 +0200

    Add files via upload

--- SurvivalAnalysisKaplanMeierEstimatorDemo.R
+++ SurvivalAnalysisKaplanMeierEstimatorDemo.R
@@ -1,29 +1,23 @@
----
-title: "Kaplan–Meier estimator Notebook"
-output: html_notebook
----
+# This notebook illustrates the effectiveness of survival analysis statistics to model event based processes.
+# The Kaplan-Meier estimator is used to obtain a survival function using a sample of random data generated from a weibull distribution. This script can be easily adapted for other distributions.
 
-This notebook illustrates the effectiveness of survival analysis statistics to model event based processes.
-The Kaplan-Meier estimator is used to obtain a survival function using a sample of random data generated from a weibull distribution. This script can be easily adapted for other distributions.
+# First the required R packages are loaded.
+# Next a number of functions are defined. This first generates random data based on a (weibull) distribution function (generate_deal). Next is a function which is used to display the obtained results and the theoretical expected values (plot_km_vs_theoretical_value). The final 2 functions retrieve the period event rate and the cumulative event rate from the (weibull) distribution function.
 
-First the required R packages are loaded.
-Next a number of functions are defined. This first generates random data based on a (weibull) distribution function (generate_deal). Next is a function which is used to display the obtained results and the theoretical expected values (plot_km_vs_theoretical_value). The final 2 functions retrieve the period event rate and the cumulative event rate from the (weibull) distribution function.
-
-```{r}
 library(ggplot2)
 library(dplyr)
 library(survival)
 
 generate_deal <- function(nbr, end_period = 3650, horizon_period = 3650, minimum_duration = 1825, density_function = 0.0001) {
-#' This function generates a number (nbr) of deals based on the density function (default_density) and returns these as a data frame.
-#' Each of the deals starts at a random period from 1 to end_period. Each of the deals has a minimum original maturity of minimum_duration. The horizon_period is the period at which the deals are being observed (aka reporting date).
-#' The data frame contains 6 columns.
-#' The end period is the value of the input variable which is the latest period at which a deal can start.
-#' The deal period is a random period between period 1 and the end period at which the deal starts.
-#' The time value is the number of periods after the deal period that a deal defaulted or matured or when the horizon period was reached (which ever occured first)
-#' The censor value indicates if the deal was censored (id est the deal did not default at the deal period + time)
-#' The maturity period is a random period between the deal period and the end period increased with the deal period and the minimum duration. It is the period in which the deal will mature if no default occured before that period.
-#' The default period is the period when a deal defaulted. If it did not default the value is set to -1.
+  #' This function generates a number (nbr) of deals based on the density function (default_density) and returns these as a data frame.
+  #' Each of the deals starts at a random period from 1 to end_period. Each of the deals has a minimum original maturity of minimum_duration. The horizon_period is the period at which the deals are being observed (aka reporting date).
+  #' The data frame contains 6 columns.
+  #' The end period is the value of the input variable which is the latest period at which a deal can start.
+  #' The deal period is a random period between period 1 and the end period at which the deal starts.
+  #' The time value is the number of periods after the deal period that a deal defaulted or matured or when the horizon period was reached (which ever occured first)
+  #' The censor value indicates if the deal was censored (id est the deal did not default at the deal period + time)
+  #' The maturity period is a random period between the deal period and the end period increased with the deal period and the minimum duration. It is the period in which the deal will mature if no default occured before that period.
+  #' The default period is the period when a deal defaulted. If it did not default the value is set to -1.
   # Check that the horizon period is at least equal to the end period
   if (horizon_period < end_period) {
     print('The horizon period can not be smaller than the end period. It has been set equal to the horizon period for the generation of data.')
@@ -62,7 +56,7 @@ generate_deal <- function(nbr, end_period = 3650, horizon_period = 3650, minimum
 }
 
 plot_km_vs_theoretical_value <- function(KM, horizon, cumulative_density_function) {
-# Function to plot the Kaplan-Meier estimator and the expected values of the cumulative density function.
+  # Function to plot the Kaplan-Meier estimator and the expected values of the cumulative density function.
   plot(KM, xlim = c(0,horizon), ylim = c(0,1), col = 'blue', xaxs = 'r') # plots the KM with 95% confidence intervals
   par(new=TRUE)
   xi = seq(0, horizon, length=10*horizon)
@@ -81,10 +75,9 @@ get_period_cumulative_event_rate_weibull <- function(period, k = k_value, lambda
   return(pweibull(period,k,scale = lambda))
 }
 
-```
-Below are listed the input parameters of the data generation process and of the distribution function. These values can be modified to examine the accuracy of the Kaplan-Meier estimator for other Weibull distributions. The corresponding Weibull distribution is displayed.
-The value of period_of_observation should be at least equal to the value of deal_latest_period_to_start.
-```{r}
+# Below are listed the input parameters of the data generation process and of the distribution function. These values can be modified to examine the accuracy of the Kaplan-Meier estimator for other Weibull distributions. The corresponding Weibull distribution is displayed.
+# The value of period_of_observation should be at least equal to the value of deal_latest_period_to_start.
+
 deal_latest_period_to_start <- 200
 period_of_observation <- 200
 number_of_deals <- 25
@@ -100,12 +93,12 @@ y <- dweibull(x,k_value,scale = lambda_value) # density
 data <- as.data.frame(cbind(x,y))
 plot <- ggplot(data, aes(x = data$x, y = data$y))
 print(plot + geom_line() + xlab(paste("k = ", k_value, " ; lambda = ", lambda_value)) + ylab("Weibull density"))
-```
-Below is the main section of the notebook.
-First the data generation function is called (modify the nbr parameter to get a different sample size).
-In the next 2 lines a Kaplan-Meier estimator model is generated.
-In the last line the results display function is called which allows to get a feeling of the accuracy of the obtained survival function compared to the theoretical survival function.
-```{r}
+
+# Below is the main section of the notebook.
+# First the data generation function is called (modify the nbr parameter to get a different sample size).
+# In the next 2 lines a Kaplan-Meier estimator model is generated.
+# In the last line the results display function is called which allows to get a feeling of the accuracy of the obtained survival function compared to the theoretical survival function.
+
 # generate data
 data <- generate_deal(nbr = number_of_deals, end_period = deal_latest_period_to_start, horizon_period = period_of_observation, minimum_duration = deal_minimum_duration, density_function = period_event_rate)
 
@@ -115,4 +108,3 @@ KM=survfit(data_surv~1) # Obtain the Kaplan-Meier estimator
 
 # Kaplan Meier analysis
 plot_km_vs_theoretical_value(KM = KM, horizon = period_of_observation, cumulative_density_function = period_event_cumulative_rate)
-```
